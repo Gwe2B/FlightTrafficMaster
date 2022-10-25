b7e513d7de359d1b457f2aadd018c2df3340661d
Gwe2B <gwenael2b@live.fr>
2022-10-25 15:33:47 +0200

    Creating the flights recycled list view
    Creating the flights recycled list view and binding it with the datas.
    Left to do: recreate it under fragment form

--- app/src/main/java/com/flightmasterteam/flightmaster/FlightListActivity.kt
+++ app/src/main/java/com/flightmasterteam/flightmaster/FlightListActivity.kt
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.LinearLayout
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class FlightListActivity : AppCompatActivity() {
+class FlightListActivity : AppCompatActivity(), FlightListAdapter.OnCellClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flight_list)
@@ -19,5 +23,24 @@ class FlightListActivity : AppCompatActivity() {
 
         val viewModel = ViewModelProvider(this).get(FlightListViewModel::class.java)
         viewModel.doRequest(begin, end, isArrival, icao!!)
+
+        viewModel.getFlightListLiveData().observe(this, Observer {
+            // Retrieve the recycler view
+            var recyclerView = findViewById<RecyclerView>(R.id.flights_recycler_view)
+
+            // Attach an Adapter
+            recyclerView.adapter = FlightListAdapter(it, this)
+
+            // Attach a LayoutManager
+            recyclerView.layoutManager = LinearLayoutManager(
+                this,
+                LinearLayoutManager.VERTICAL,
+                false
+            )
+        })
+    }
+
+    override fun onCellClicked(flightModel: FlightModel) {
+        TODO("Not yet implemented")
     }
 }
---
+++ app/src/main/java/com/flightmasterteam/flightmaster/FlightListAdapter.kt
@@ -0,0 +1,44 @@
+package com.flightmasterteam.flightmaster
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class FlightListAdapter(
+        private val dataSet: List<FlightModel>,
+        private val cellClickListener: OnCellClickListener
+    ): RecyclerView.Adapter<FlightListAdapter.ViewHolder>() {
+
+    interface OnCellClickListener {
+        fun onCellClicked(flightModel: FlightModel)
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val cell = FlightCell(parent.context)
+        cell.layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+
+        return ViewHolder(cell)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val flight = dataSet[position]
+        val cell = holder.itemView as FlightCell
+        cell.setBackgroundColor(if(position%2 == 0) Color.YELLOW else Color.GRAY)
+
+        cell.bindData(flight)
+        cell.setOnClickListener() {
+            cellClickListener.onCellClicked(flight)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
+}
